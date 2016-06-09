7cf9a9a481676f3fc5a62738f06459d501eb4ab5
Dan Radez <dradez@redhat.com>
2016-06-09 14:12:17 -0400

    updating db_sync command
    Signed-off-by: Dan Radez <dradez@redhat.com>

--- manifests/db/sync.pp
+++ manifests/db/sync.pp
@@ -4,6 +4,7 @@
 class congress::db::sync {
   exec { 'congress-manage db_sync':
     path        => '/usr/bin',
+    command     => 'congress-db-manage --config-file /etc/congress/congress.conf upgrade head',
     user        => 'congress',
     refreshonly => true,
     subscribe   => [Package['congress'], congress_config['database/connection']],
